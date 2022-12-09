47c70e9436691469f40d2f6ec357837a96c4108d
Nevin Alisheva <nevin.alisheva@gmail.com>
2022-12-10 00:47:42 +0100

    created handlers to delete quest 5

--- userHandlers.js
+++ userHandlers.js
@@ -68,4 +68,21 @@ const updateUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, getUserById, postUser, updateUser };
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  database
+    .query("DELETE FROM users WHERE id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting user");
+    });
+};
+
+module.exports = { getUsers, getUserById, postUser, updateUser, deleteUser };
